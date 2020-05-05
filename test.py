daff8cdad5cead45c35b58441fafb682ee03abd9
Abel <abelttitus1998@gmail.com>
2020-05-05 12:50:22 +0530

    Mesh_vis

--- test.py
+++ test.py
@@ -3,14 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-# monkey patches visualization and provides helpers to load geometries
-sys.path.append('..')
-import open3d_tutorial as o3dtut
-# change to True if you want to interact with the visualization windows
-o3dtut.interactive = False
 
-print("Read Container Image")
-color_raw = o3d.io.read_image("/home/abel/pang_tut/container.jpg")
-plt.title("Image")
-plt.imshow(color_raw)
-plt.show()
+
+mesh = o3d.io.read_traingle_mesh("/home/abel/open3d_tut/bathtub_0154.ply")
+o3d.visualization.draw_geometries([mesh])
